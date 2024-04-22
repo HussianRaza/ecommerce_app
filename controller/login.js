46c1cb959466e7fab9a12168e9c8ee3805376791
Hussain <razahussain876@gmail.com>
2024-04-22 19:47:08 +0500

    add cart id to session storage in login

--- controller/login.js
+++ controller/login.js
@@ -1,5 +1,11 @@
-const loginUser = (req, res) => {
-  res.send("it works");
+import db from "../model/db";
+
+const loginUser = async (req, res) => {
+  const { id } = req.session.passport.user;
+
+  const dbRes = await db.query("SELECT id FROM cart WHERE user_id = $1", [id]);
+
+  req.session.cartId = dbRes.rows[0].id;
 };
 
 export { loginUser };
