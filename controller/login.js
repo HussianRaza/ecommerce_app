e809b06c6eed2208518e3ff9c18389d490ae3519
Hussain <razahussain876@gmail.com>
2024-04-23 21:33:24 +0500

    fix infinite loading in postman

--- controller/login.js
+++ controller/login.js
@@ -6,6 +6,9 @@ const loginUser = async (req, res) => {
   const dbRes = await db.query("SELECT id FROM cart WHERE user_id = $1", [id]);
 
   req.session.cartId = dbRes.rows[0].id;
+
+
+  return res.send("success login")
 };
 
 export { loginUser };
