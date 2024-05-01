e4e28f374508ce8d5a4f3a302aed712c8c5ada41
Hussain <razahussain876@gmail.com>
2024-05-01 22:09:36 +0500

    register component

---
+++ view/src/pages/authentication/Register.jsx
@@ -0,0 +1,150 @@
+import React, { useState } from "react";
+import { registerUser } from "../../utils";
+import RegisterBanner from "./RegisterBanner";
+
+function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  const handleSumbmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const res = await registerUser({ email, password });
+    setEmail("");
+    setPassword("");
+    setIsLoading(false);
+
+    if (res.status == 200) {
+      setIsCompleted(true);
+    }
+  };
+
+  return (
+    <>
+      {isCompleted ? (
+        <RegisterBanner />
+      ) : (
+        <div className="bg-white dark:bg-gray-900">
+          <div className="flex justify-center h-screen">
+            <div
+              className="hidden bg-cover lg:block lg:w-2/3"
+              style={{
+                backgroundImage:
+                  "url(https://images.unsplash.com/photo-1608231387042-66d1773070a5?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+              }}
+            >
+              <div className="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
+                <div>
+                  <h2 className="text-2xl font-bold text-white sm:text-3xl">
+                    Shoe Shop.
+                  </h2>
+                  <p className="max-w-xl mt-3 text-white font-bold">
+                    Discover the perfect pair for every step of your journey.
+                    From sleek and sophisticated to casual comfort, our
+                    collection has something for every style and occasion. Step
+                    up your footwear game with us today!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
+              <div className="flex-1">
+                <div className="text-center">
+                  <div className="flex justify-center mx-auto">
+                    <img
+                      className="w-auto h-20 sm:h-18"
+                      src="shoe.svg"
+                      alt=""
+                    />
+                  </div>
+                  <p className="mt-3 text-gray-500 dark:text-gray-300">
+                    Create An Account For Shoe Shop{" "}
+                  </p>
+                </div>
+                <div className="mt-8">
+                  <form onSubmit={handleSumbmit}>
+                    <div>
+                      <label
+                        htmlFor="email"
+                        className="block mb-2 text-sm text-gray-600 dark:text-gray-200"
+                      >
+                        Email Address
+                      </label>
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="example@example.com"
+                        required
+                        className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                      />
+                    </div>
+                    <div className="mt-6">
+                      <div className="flex justify-between mb-2">
+                        <label
+                          htmlFor="password"
+                          className="text-sm text-gray-600 dark:text-gray-200"
+                        >
+                          Password
+                        </label>
+                      </div>
+                      <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Your Password"
+                        minLength={5}
+                        required
+                        className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                      />
+                    </div>
+                    <div className="mt-6">
+                      {isLoading ? (
+                        <button className="flex justify-center items-center  w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                          <svg
+                            width="20"
+                            height="20"
+                            fill="currentColor"
+                            className="mr-2 animate-spin"
+                            viewBox="0 0 1792 1792"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path d="M526 1394q0 53-37.5 90.5t-90.5 37.5q-52 0-90-38t-38-90q0-53 37.5-90.5t90.5-37.5 90.5 37.5 37.5 90.5zm498 206q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-704-704q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm1202 498q0 52-38 90t-90 38q-53 0-90.5-37.5t-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-964-996q0 66-47 113t-113 47-113-47-47-113 47-113 113-47 113 47 47 113zm1170 498q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-640-704q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm530 206q0 93-66 158.5t-158 65.5q-93 0-158.5-65.5t-65.5-158.5q0-92 65.5-158t158.5-66q92 0 158 66t66 158z"></path>
+                          </svg>
+                          Sign Up
+                        </button>
+                      ) : (
+                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                          {" "}
+                          Sign Up
+                        </button>
+                      )}
+                    </div>
+                  </form>
+                  <p className="mt-6 text-sm text-center text-gray-400">
+                    Already have an account?{" "}
+                    <a
+                      href="/"
+                      className="text-blue-500 focus:outline-none focus:underline hover:underline"
+                    >
+                      Sign in
+                    </a>
+                    .
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Register;
